b9154d50a6ac024ecdd41613310a06bfff68b3be
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-03-19 01:23:54 -0400

    Added banner image

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import './css/App.css';
+import skyImg from './assets/banner-img.webp';
+
 function App() {
 
   return (
-    <>
+    <div className="app">
+      <div className="banner">
+        <img src={skyImg} alt="clear sky"/>
+      </div>
 
-    </>
+    </div>
   )
 }
 

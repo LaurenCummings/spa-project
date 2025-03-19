81fcef164d42d7daa0d53728fcac93923e700eac
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-03-19 02:22:02 -0400

    Added banner text with styling

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,10 @@ function App() {
     <div className="app">
       <div className="banner">
         <img src={skyImg} alt="clear sky"/>
+        <h1 className="welcome">welcome</h1>
+        <h1 className="to">to</h1>
+        <h1 className="relaxation">relaxation</h1>
       </div>
-
     </div>
   )
 }

8cc798ca8059af0e01bc0475cce6af9f517f0600
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-03-21 03:22:00 -0400

    Added service nav blocks

--- src/components/ServiceNav.jsx
+++ src/components/ServiceNav.jsx
@@ -4,6 +4,20 @@ function ServiceNav() {
     return (
         <div className="service-nav">
             <h1>Services</h1>
+            <div className="services">
+                <div className="service">
+                    <h2>Massage</h2>
+                </div>
+                <div className="service">
+                    <h2>Skincare</h2>
+                </div>
+                <div className="service">
+                    <h2>Nails</h2>
+                </div>
+                <div className="service">
+                    <h2>Body Treatments</h2>
+                </div>
+            </div>
         </div>
     )
 }

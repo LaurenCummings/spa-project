c6338a3048cd060c7a48d7bf43f10dd21090a57c
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-03-21 02:24:46 -0400

    Added code for footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './css/App.css';
 import Banner from './components/Banner.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
 
   return (
     <div className="app">
       <Banner />
+      {/* <Footer /> */}
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import '../css/Footer.css';
+
+function Footer() { 
+    return (
+        <div className="footer">
+            <p><a target="_blank" href="https://icons8.com/icon/Vhgkr6lfmGL8/water">Water</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a></p>
+        </div>
+    )
+}
+
+export default Footer;

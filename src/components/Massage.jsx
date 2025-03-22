7ba1e9233d031709be43fa3585a977c652ac4bf9
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-03-22 02:27:18 -0400

    added more details to massage services

--- src/components/Massage.jsx
+++ src/components/Massage.jsx
@@ -8,22 +8,32 @@ function Massage() {
                 <div className="massage-service">
                     <h2>Swedish Massage</h2>
                     <p>Paragraph describing Swedish Massage</p>
+                    <p>30 minutes $90</p>
+                    <p>60 minutes $150</p>
                 </div>
                 <div className="massage-service">
                     <h2>Deep Tissue Massage</h2>
                     <p>Paragraph describing Deep Tissue Massage</p>
+                    <p>30 minutes $90</p>
+                    <p>60 minutes $150</p>
                 </div>
                 <div className="massage-service">
                     <h2>Foot Massage</h2>
                     <p>Paragraph describing Foot Massage</p>
+                    <p>30 minutes $90</p>
+                    <p>60 minutes $150</p>
                 </div>
                 <div className="massage-service">
                     <h2>Hot Stone Massage</h2>
                     <p>Paragraph describing Hot Stone massage</p>
+                    <p>30 minutes $90</p>
+                    <p>60 minutes $150</p>
                 </div>
                 <div className="massage-service">
                     <h2>Back and Shoulder Massage</h2>
                     <p>Paragraph describing Back and Shoulder Massage</p>
+                    <p>30 minutes $90</p>
+                    <p>60 minutes $150</p>
                 </div>
             </div>
         </div>

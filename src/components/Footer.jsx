0fb171f40ff0b69879e26b0f841b270b5d7285d9
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-03-24 02:03:17 -0400

    Added location to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,11 @@ import '../css/Footer.css';
 function Footer() { 
     return (
         <div className="footer">
+            <div className="location">
+                <p>We are located at</p>
+                <p>2800 Fake St</p>
+                <p>Faketown, FakeState</p>
+            </div>
             <p><a target="_blank" href="https://icons8.com/icon/Vhgkr6lfmGL8/water">Water</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a></p>
         </div>
     )

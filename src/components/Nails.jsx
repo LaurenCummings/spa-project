6d64951f6263e2520f507739550f2d431b652410
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-03-23 22:44:04 -0400

    Added content and styled nails

--- src/components/Nails.jsx
+++ src/components/Nails.jsx
@@ -2,8 +2,25 @@ import '../css/Nails.css';
 
 function Nails() {
     return (
-        <div>
-
+        <div className="nails">
+            <h1>Nails</h1>
+            <div className="nail-treatments">
+                <div className="nail-treatment">
+                    <h2>Manicure</h2>
+                    <p>Description of manicure</p>
+                    <p>price</p>
+                </div>
+                <div className="nail-treatment">
+                    <h2>Pedicure</h2>
+                    <p>Description of pedicure</p>
+                    <p>price</p>
+                </div>
+                <div className="nail-treatment">
+                    <h2>Package Deal</h2>
+                    <p>a manicure and pedicure combined</p>
+                    <p>price</p>
+                </div>
+            </div>
         </div>
     )
 }

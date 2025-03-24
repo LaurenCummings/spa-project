aef8c05ea8c36cb51b47e34e5a1e45ff8248a359
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-03-23 22:29:27 -0400

    Added image to skincare and styled

--- src/components/Skincare.jsx
+++ src/components/Skincare.jsx
@@ -1,11 +1,15 @@
 import '../css/Skincare.css';
+import faceMask from '../assets/facemask.jpg';
 
 function Skincare() {
     return (
         <div className="skincare">
-            <h1>Skincare</h1>
-            <p>Our 1 hour facial treatment starts off with a facial cleansing mask and steam treatment, followed by dermaplaning and a jelly mask. We end the treatment with a face and neck massage.</p>
-            <p>60 minutes $120</p>
+            <div className="skincare-info">
+                <h1>Skincare</h1>                
+                <p>Our 1 hour facial treatment starts off with a facial cleansing mask and steam treatment, followed by dermaplaning and a jelly mask. We end the treatment with a face and neck massage.</p>
+                <p>60 minutes $120</p>                
+            </div>
+            <img src={faceMask} alt="face mask" />
         </div>
     )
 }

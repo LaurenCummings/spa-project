8681a86746ce95d79ea86bfef9e5bc7db11f5716
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-03-27 00:55:08 -0400

    Added nail treatment descriptions

--- src/components/Nails.jsx
+++ src/components/Nails.jsx
@@ -8,18 +8,18 @@ const Nails = forwardRef((props, ref) => {
             <div className="nail-treatments">
                 <div className="nail-treatment">
                     <h2>Manicure</h2>
-                    <p>Description of manicure</p>
-                    <p>price</p>
+                    <p>This treatment involves shaping and filing the nails and caring for the cuticles. Afterwards, the nails and buffed and your choice of polish and patterns will be applied.</p>
+                    <p>60 minutes $60</p>
                 </div>
                 <div className="nail-treatment">
                     <h2>Pedicure</h2>
-                    <p>Description of pedicure</p>
-                    <p>price</p>
+                    <p>We will remove old polish and soak your feet in warm water before applying new polish. Afterwards, your feet will be moisturized and the procedure will finish with a foot massage.</p>
+                    <p>60 minutes $70</p>
                 </div>
                 <div className="nail-treatment">
                     <h2>Package Deal</h2>
-                    <p>a manicure and pedicure combined</p>
-                    <p>price</p>
+                    <p>Relax while you are pampered with both a manicure and pedicure</p>
+                    <p>60 minutes $100</p>
                 </div>
             </div>
         </div>

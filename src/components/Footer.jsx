e01c44adac69c71898c8619749cd7a3289ba7573
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-22 22:44:16 -0400

    added styling to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,14 @@ function Footer() {
     return (
         <div className="footer">
             <div className="location">
-                <p>We are located at</p>
+                <p>We are located at:</p>
                 <p>2800 Fake St</p>
                 <p>Faketown, FakeState</p>
             </div>
-            <p><a target="_blank" href="https://icons8.com/icon/Vhgkr6lfmGL8/water">Water</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a></p>
-            <p>Service descriptions created with the help of Google AI Overview</p>
+            <div className="extra-info">
+                <p><a target="_blank" href="https://icons8.com/icon/Vhgkr6lfmGL8/water">Water</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a></p>
+                <p>Service descriptions created with the help of Google AI Overview</p>
+            </div>
         </div>
     )
 }

84a1df84d144149adf0f188e0024812534e153f3
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-23 23:07:03 -0400

    made book an appointment button scroll to bottom of page

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -10,13 +10,20 @@ function Topbar() {
         });
     }
 
+    function scrollToBottom() {
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: "smooth"
+        })
+    }
+
     return(
         <div className="topbar">
             <div className="logo">
                 <img src={logo} alt="logo" />
                 <h1>The Spa</h1>
             </div>
-            <button className="book-appt">Book an Appointment</button>
+            <button className="book-appt" onClick={scrollToBottom}>Book an Appointment</button>
             <button className="scroll-top" onClick={scrollToTop}>Scroll to Top</button>
         </div>
     )

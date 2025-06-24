bd166d8146ea0b38071a4f369ea4d67ebac702c0
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-23 23:44:54 -0400

    started structuring appointment info

--- src/components/BookNow.jsx
+++ src/components/BookNow.jsx
@@ -10,10 +10,22 @@ function BookNow() {
             </div>
             <div className="appointment-info">
                 <h3>Available Appointments</h3>
-                <p>10am</p>
-                <p>1pm</p>
-                <p>2pm</p>
-                <p>3pm</p>
+                <div className="appointment">
+                    <p>10am</p>
+                    <button>Select</button>
+                </div>
+                <div className="appointment">
+                    <p>1pm</p>
+                    <button>Select</button>
+                </div>
+                <div className="appointment">
+                    <p>2pm</p>
+                    <button>Select</button>
+                </div>
+                <div className="appointment">
+                    <p>3pm</p>
+                    <button>Select</button>
+                </div>
                 <button>Book Now</button>
             </div>
         </div>

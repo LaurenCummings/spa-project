074760485bf0326e2be1f1c82fc077baf7b69355
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-24 22:58:51 -0400

    created dummy data for appointments

---
+++ src/Data.js
@@ -0,0 +1,12 @@
+const appointments = [
+    {
+        day: "odd",
+        times: ["9am", "11am", "1pm", "2pm", "4pm"],
+    },
+    {
+        day: "even",
+        times: ["8am", "10am", "11am", "3pm", "4pm"],
+    }
+]
+
+export default appointments;

16d1fff803c4f83a9c9caa8bc934354a0d442767
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-26 22:29:44 -0400

    changed one more selectedTime to selectedTimeIndex

--- src/components/BookNow.jsx
+++ src/components/BookNow.jsx
@@ -12,7 +12,7 @@ function BookNow() {
     const [chosenAppt, setChosenAppt] = useState();
 
     const handleSelectedTime = (index) => {
-        setSelectedTime(index);
+        setSelectedTimeIndex(index);
     }
 
     const handleSelectedDate = (selectedDate) => {

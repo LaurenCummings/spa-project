c5bb4b4173e32bc5ac8efd24abfd08bc4bfcec26
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-28 22:04:19 -0400

    created file for service data

---
+++ src/Services.jsx
@@ -0,0 +1,10 @@
+export const massageServices = [
+    {
+        id: 1,
+        service: "Swedish Massage",
+        description: "This massage uses a variety of strokes and techniques to promote relaxation, improve circulation, and relieve muscle tension",
+        span: "60 minutes",
+        price: "$150",
+    },
+    
+]

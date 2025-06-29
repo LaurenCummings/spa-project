930d7dc47a8616ea36e12176d7f979809fc6ffb0
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-28 22:56:44 -0400

    mapped out body services using data file

--- src/components/BodyTreatments.jsx
+++ src/components/BodyTreatments.jsx
@@ -2,12 +2,27 @@ import '../css/BodyTreatments.css';
 import bodyscrub from '../assets/bodyscrub.jpg';
 import seaweedwrap from '../assets/seaweedwrap.jpg';
 import { forwardRef } from 'react';
+import { bodyServices } from '../Services.js';
 
 const BodyTreatments = forwardRef((props, ref) => {
     return (
         <div ref={ref} className="body-treatments">
             <h1>Body Treatments</h1>
             <div className="body-treatment-services">
+                {
+                    bodyServices.map((service) => {
+                        return (
+                            <div key={service.id} className="body-treatment-service">
+                                <img src={service.image} alt={service.name} />
+                                <div>
+                                    <h2>{service.name}</h2>
+                                    <p>{service.description}</p>
+                                    <p>{service.duration} {service.price}</p>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
                 <div className="body-treatment-service">
                     <img src={bodyscrub} alt="jar of body scrub" />
                     <div>

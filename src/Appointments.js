e31f6804db08c82c4fb5f72365b03327b3e7e728
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-29 21:22:49 -0400

    added weekday appointments to data file

--- src/Appointments.js
+++ src/Appointments.js
@@ -53,3 +53,143 @@ export const evenAppointments = [
         time: "4pm",
     },
 ]
+
+export const monAppts = [
+    {
+        id: 1,
+        tech: "Alice",
+        time: "9am",
+    },
+    {
+        id: 2,
+        tech: "Mark",
+        time: "11am",
+    },
+    {
+        id: 3,
+        tech: "Alice",
+        time: "1pm",
+    },
+    {
+        id: 4,
+        tech: "Amber",
+        time: "2pm",
+    },
+    {
+        id: 5,
+        tech: "Mark",
+        time: "4pm",
+    },
+]
+
+export const tueAppts = [
+    {
+        id: 6,
+        tech: "Susie",
+        time: "8am",
+    },
+    {
+        id: 7,
+        tech: "Amber",
+        time: "10am",
+    },
+    {
+        id: 8,
+        tech: "Jim",
+        time: "11am",
+    },
+    {
+        id: 9,
+        tech: "Susie",
+        time: "3pm",
+    },
+    {
+        id: 10,
+        tech: "Mark",
+        time: "4pm",
+    },
+]
+
+export const wedAppts = [
+    {
+        id: 6,
+        tech: "Susie",
+        time: "8am",
+    },
+    {
+        id: 7,
+        tech: "Amber",
+        time: "10am",
+    },
+    {
+        id: 8,
+        tech: "Jim",
+        time: "11am",
+    },
+    {
+        id: 9,
+        tech: "Susie",
+        time: "3pm",
+    },
+    {
+        id: 10,
+        tech: "Mark",
+        time: "4pm",
+    },
+]
+
+export const thuAppts = [
+    {
+        id: 6,
+        tech: "Susie",
+        time: "8am",
+    },
+    {
+        id: 7,
+        tech: "Amber",
+        time: "10am",
+    },
+    {
+        id: 8,
+        tech: "Jim",
+        time: "11am",
+    },
+    {
+        id: 9,
+        tech: "Susie",
+        time: "3pm",
+    },
+    {
+        id: 10,
+        tech: "Mark",
+        time: "4pm",
+    },
+]
+
+export const friAppts = [
+    {
+        id: 6,
+        tech: "Susie",
+        time: "8am",
+    },
+    {
+        id: 7,
+        tech: "Amber",
+        time: "10am",
+    },
+    {
+        id: 8,
+        tech: "Jim",
+        time: "11am",
+    },
+    {
+        id: 9,
+        tech: "Susie",
+        time: "3pm",
+    },
+    {
+        id: 10,
+        tech: "Mark",
+        time: "4pm",
+    },
+]
